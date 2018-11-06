50066e6860a35049bdb6ec024e9819ceaff18b5f
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-06 12:23:51 -0500

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/jazz.jpeg') {
+    if(mySrc === 'images/headshot.jpg') {
       myImage.setAttribute ('src','images/jazzdevil.gif');
     } else {
-      myImage.setAttribute ('src','images/jazz.jpeg');
+      myImage.setAttribute ('src','images/headshot.jpg');
     }
 }
 
@@ -15,14 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    myHeading.textContent = 'Hello, ' + myName;
   }
 
   if(!localStorage.getItem('name')) {
     setUserName();
   } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    myHeading.textContent = 'Hello, ' + storedName + ". It's time you learn about Sean McQuaid!";
   }
 
   myButton.onclick = function() {
